409890c9fcb38475e9cd70c1972024198dc1ed37
estudiante <rucamont@espol.edu.ec>
2023-01-13 15:55:25 -0500

    first commit

---
+++ app/src/main/java/com/example/amst_lab07_grupo4/PerfilUsuario.java
@@ -0,0 +1,141 @@
+package com.example.amst_lab07_grupo4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PerfilUsuario extends AppCompatActivity implements View.OnClickListener{
+    TextView txt_id, txt_name, txt_email, txt_telefono;
+    Button tweet;
+    EditText txt_tweet, txt_date;
+    ImageView imv_photo;
+    DatabaseReference db_reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+
+        Intent intent = getIntent();
+        HashMap<String, String> info_user = (HashMap<String, String>)intent.getSerializableExtra("info_user");
+        txt_telefono=findViewById(R.id.txt_phone);
+        txt_id = findViewById(R.id.txt_userId);
+        txt_name = findViewById(R.id.txt_nombre);
+        txt_email = findViewById(R.id.txt_correo);
+        imv_photo = findViewById(R.id.imv_foto);
+
+        txt_date=findViewById(R.id.txt_date);
+        txt_tweet=findViewById(R.id.txt_tweet);
+        tweet= (Button) findViewById(R.id.btn_tweet);
+
+        txt_id.setText(info_user.get("user_id"));
+        txt_name.setText(info_user.get("user_name"));
+        txt_email.setText(info_user.get("user_email"));
+        txt_telefono.setText(info_user.get("user_phone"));
+        String photo = info_user.get("user_photo");
+        Picasso.get().load(photo).into(imv_photo);
+
+        iniciarBaseDeDatos();
+        //leerTweets();
+        //tweet.setOnClickListener(this);
+
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            HashMap<String, String> info_user = new HashMap<String, String>();
+            info_user.put("user_name", user.getDisplayName());
+            info_user.put("user_email", user.getEmail());
+            info_user.put("user_photo", String.valueOf(user.getPhotoUrl()));
+            info_user.put("user_id", user.getUid());
+
+            finish();
+            Intent intent = new Intent(this, PerfilUsuario.class);
+            intent.putExtra("info_user", info_user);
+            startActivity(intent);
+
+        } else {
+            System.out.println("sin registrarse");
+        }
+    }
+
+    public void cerrarSesion(View view){
+        FirebaseAuth.getInstance().signOut();
+        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("msg", "cerrarSesion");
+        startActivity(intent);
+    }
+
+    public void iniciarBaseDeDatos(){
+        db_reference = FirebaseDatabase.getInstance().getReference().child("Grupos");
+    }
+
+   /* public void leerTweets(){
+        db_reference.child("Grupo0").child("tweets")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            System.out.println(snapshot);
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        System.out.println(error.toException());
+                    }
+                });
+    }*/
+
+   /* public void escribirTweets(String autor, String txt_date, String txt_tweet){
+
+
+        String tweet = txt_tweet;
+        String fecha = txt_date; //Fecha actual
+        Map<String, String> hola_tweet = new HashMap<String, String>();
+        hola_tweet.put("autor", autor);
+        hola_tweet.put("fecha", fecha);
+        DatabaseReference tweets = db_reference.child("Grupo2").child("tweets");
+        tweets.setValue(tweet);
+        tweets.child(tweet).child("autor").setValue(autor);
+        tweets.child(tweet).child("fecha").setValue(fecha);
+
+    }*/
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = getIntent();
+        HashMap<String, String> info_user = (HashMap<String, String>)intent.getSerializableExtra("info_user");
+
+        //escribirTweets(info_user.get("user_name"),txt_date.getText().toString(),txt_tweet.getText().toString());
+        Toast mensaje = Toast.makeText(getApplicationContext(),"Se envio el Tweet", Toast.LENGTH_SHORT);
+        mensaje.show();
+        txt_date.setText("");
+        txt_tweet.setText("");
+    }
+
+    public void irRegistros(View view){
+        Intent intent = new Intent(this, registros.class);
+        startActivity(intent);
+    }
+}
